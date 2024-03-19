0b9f0c20fb865f092ce32074b7c0a4be33c1cb5c
Kartik  Kohapare <87128102+9834897332@users.noreply.github.com>
2024-03-19 20:12:30 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,27 @@
+// function test()
+// {
+//     var num = 100;
+//     console.log("value of num in test()" +num);
+//     {
+//         console.log("Inner Block begins")
+//         let num  = 200;
+        
+//         console.log("value of num:" +num)
+//     }
+// }
+
+
+/* let count = 100;
+for(let count = 1; count <= 10; count++)
+{
+    console.log("count value inside loop is ",count);
+
+}
+console.log("count value after loop is ",count);
+
+var str = 'hello universe !!!';
+console.log(str.endsWith('hello'));
+console.log(str.endsWith('hello,5'));
+
+document.write("hello world")
+*/
